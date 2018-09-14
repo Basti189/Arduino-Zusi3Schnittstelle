df8327440281cd9fb788fc3cf3f41905d9761626
Sebastian Wolf <Basti189@users.noreply.github.com>
2018-09-14 02:22:26 +0200

    Option Programmdaten ohne Fahrplan hinzugefügt
    Wenn die Fahrplandatei zu lang ist stürzt der Mikrocontroller ab

--- Arduino/Zusi3Schnittstelle.cpp
+++ Arduino/Zusi3Schnittstelle.cpp
@@ -3,263 +3,97 @@
 * Created:	August 2018
 */
 
-#include "Zusi3Schnittstelle.h"
+#ifndef ZUSI3SCHNITTSTELLE_H
+#define ZUSI3SCHNITTSTELLE_H
 
-Zusi3Schnittstelle::Zusi3Schnittstelle(String ip, int port, String clientName) {
-	this->ip = ip;
-	this->port = port;
-	this->clientName = clientName;
-#if defined(ESP8266_Wifi) || defined(ESP32_Wifi)
-	client = new WiFiClient();
+#include "Node.h"
+#include "Fuehrerstandsanzeigen.h"
+#ifndef LinkedList_h
+#error Bitte lade im Bibliotheksverwalter <LinkedList by Ivan Seidel> in der Version 1.2.3 herunter
 #endif
-#ifdef ESP32_Ethernet
-	client = new WiFiClient();
-#endif
-#ifdef Ethernet_Shield
-	client = new EthernetClient();
-#endif
-#ifdef AVR_Wifi
-	client = new WiFiClient();
-#endif
-}
 
-boolean Zusi3Schnittstelle::connect() {
+#define ESP8266_Wifi //NodeMCU 1.0 - Funktioniert
+//#define ESP32_Wifi //NodeMCU-32S - Funktioniert
+//#define ESP32_Ethernet //Olimex ESP32-Gateway - Funktioniert
+//#define Ethernet_Shield //Arduino UNO hat zu wenig RAM, somit nicht getestet
+//#define AVR_Wifi // Nicht getestet
+
 #ifdef ESP8266_Wifi
-	boolean state = client->connect(ip, port);
+#include <ESP8266WiFi.h>
 #endif
 #ifdef ESP32_Wifi
-	char constCharIP[sizeof(ip)];
-	ip.toCharArray(constCharIP, sizeof(constCharIP));
-	boolean state = client->connect(constCharIP, port);
+#include <WiFi.h>
 #endif
 #ifdef ESP32_Ethernet
-	char constCharIP[sizeof(ip)];
-	ip.toCharArray(constCharIP, sizeof(constCharIP));
-	boolean state = client->connect(constCharIP, port);
+#include <ETH.h>
 #endif
 #ifdef Ethernet_Shield
-	boolean state = (boolean) client->connect(ip.c_str(), port);
+#include <SPI.h>
+#include <Ethernet.h>
 #endif
 #ifdef AVR_Wifi
-	boolean state = client->connect(ip, port);
+#include <SPI.h>
+#include <WiFi.h>
 #endif
-	if (state) {
-		HELLO();
-		ACK_HELLO();
-		NEEDED_DATA();
-		ACK_NEEDED_DATA();
-		return true;
-	}
-	return false;
-}
 
-Zusi3Schnittstelle::~Zusi3Schnittstelle() {
-	delete HEADER;
-	delete ENDE;
-	delete client;
-	delete requestListFuehrerstandsanzeigen;
-}
 
-void Zusi3Schnittstelle::close() {
-	client->stop();
-}
+class Zusi3Schnittstelle {
+public:
+	Zusi3Schnittstelle(String ip, int port, String clientName);
+	~Zusi3Schnittstelle();
+	boolean connect();
+	void close();
+	void requestFuehrerstandsanzeigen(int request);
+	void reqFstAnz(int request);
+	void requestFuehrerstandsbedienung(boolean value);
+	void requestProgrammdaten(boolean value);
+	void requestProgrammdatenOhneFahrplan(boolean value);
+	void setDebugOutput(boolean output);
+	String getZusiVersion();
+	String getVersion;
+	String getVerbindungsinfo();
+	Node *update();
 
-void Zusi3Schnittstelle::requestFuehrerstandsanzeigen(int request) {
-	requestListFuehrerstandsanzeigen->add(request);
-}
+private:
+	void HELLO();
+	void ACK_HELLO();
+	void NEEDED_DATA();
+	void ACK_NEEDED_DATA();
+	Node *getNodes(byte *rootID);
+	boolean compare(byte *a, byte *b, int size);
 
-void Zusi3Schnittstelle::reqFstAnz(int request) {
-	requestFuehrerstandsanzeigen(request);
-}
+	byte *HEADER = new byte[4]{ 0x00, 0x00, 0x00, 0x00 };
+	byte *ENDE = new byte[4]{ 0xFF, 0xFF, 0xFF, 0xFF };
 
-void Zusi3Schnittstelle::requestFuehrerstandsbedienung(boolean value) {
-	reqFuehrerstandsbedienung = value;
-}
+#if defined(ESP8266_Wifi) || defined(ESP32_Wifi)
+	WiFiClient *client;
+#endif
+#ifdef ESP32_Ethernet
+	WiFiClient *client;
+#endif
+#ifdef Ethernet_Shield
+	EthernetClient *client;
+#endif
+#ifdef AVR_Wifi
+	WiFiClient *client;
+#endif
+	LinkedList<int> *requestListFuehrerstandsanzeigen = new LinkedList<int>();
+	boolean reqFuehrerstandsbedienung = false;
+	boolean reqProgrammdaten = false;
+	boolean reqProgrammdatenOhneFahrplan = false;
 
-void Zusi3Schnittstelle::requestProgrammdaten(boolean value) {
-	reqProgrammdaten = value;
-}
+	Node *node = NULL;
 
-void Zusi3Schnittstelle::setDebugOutput(boolean output) {
-	debugOutput = output;
-}
+	String versionZusi = "unknown";
+	String verbindungsinfoZusi = "unknown";
 
-String Zusi3Schnittstelle::getZusiVersion() {
-	return versionZusi;
-}
+	String clientName;
+	String ip;
+	int port;
+	String version = "0.8.1 beta";
+	
+	boolean debugOutput = false;
+	int reconnectCounter = 1;
 
-String Zusi3Schnittstelle::getVerbindungsinfo() {
-	return verbindungsinfoZusi;
-}
-
-Node *Zusi3Schnittstelle::update() {
-	if (node != NULL) {
-		delete node;
-		node = NULL;
-	}
-	if (!client->connected()) {
-		if (debugOutput) {
-			Serial.print("Verbindungsaufbau (");
-			Serial.print(reconnectCounter);
-			Serial.print(")\n");
-		}
-		reconnectCounter++;
-		if (connect()) {
-			reconnectCounter = 1;
-		}
-	}
-	if (client->available() > 19) {
-		byte *header = new byte[4];
-		client->read(header, 4);
-		delete header;
-
-		byte *ID = new byte[2];
-		client->read(ID, 2);
-
-		node = getNodes(ID);
-		return node;
-	}
-	return NULL;
-}
-
-void Zusi3Schnittstelle::HELLO() {
-	Node *verbindungsaufbau = new Node(1);
-		Node *befehl_HELLO = new Node(1);
-			Attribute *protokoll_Version = new Attribute(1, 2);
-			Attribute *client_Typs = new Attribute(2, 2);
-			Attribute *identifikation = new Attribute(3, clientName);
-			Attribute *versionsnummer = new Attribute(4, version);
-			befehl_HELLO->addAttribute(protokoll_Version);
-		befehl_HELLO->addAttribute(client_Typs);
-		befehl_HELLO->addAttribute(identifikation);
-		befehl_HELLO->addAttribute(versionsnummer);
-	verbindungsaufbau->addNode(befehl_HELLO);
-	int length = 0;
-	byte *result = verbindungsaufbau->get(&length);
-	client->write(result, length);
-	delete result;
-	//delete verbindungsaufbau; //Absturzgrund bei dem ESP32
-}
-
-void Zusi3Schnittstelle::ACK_HELLO() {
-	while (client->available() < 10) {
-		delay(250);
-	}
-	byte *header = new byte[4];
-	client->read(header, 4);
-	byte *id = new byte[2];
-	client->read(id, 2);
-	Node *verbindungsaufbau = getNodes(id);
-	if (verbindungsaufbau->getIDAsInt() == 0x01) {
-		Node *befehl_ACK_HELLO = verbindungsaufbau->getNodeByID(0x02);
-		if (befehl_ACK_HELLO != NULL) {
-			int client_Aktzeptiert = befehl_ACK_HELLO->getAttributeByID(3)->getDATAAsInt();
-			versionZusi = befehl_ACK_HELLO->getAttributeByID(1)->getDATAAsString();
-			verbindungsinfoZusi = befehl_ACK_HELLO->getAttributeByID(2)->getDATAAsString();
-			if (debugOutput) {
-				Serial.print("client_Aktzeptiert: ");
-				Serial.println(client_Aktzeptiert);
-
-				Serial.print("zusi_version: ");
-				Serial.println(versionZusi);
-
-				Serial.print("zusi_verbindungsinfo: ");
-				Serial.println(verbindungsinfoZusi);
-			}
-		}
-	}
-	delete header;
-	delete verbindungsaufbau;
-}
-
-void Zusi3Schnittstelle::NEEDED_DATA() {
-	Node *client_Anwendung = new Node(0x02);
-		Node *befehl_NEEDED_DATA = new Node(0x03);
-			Node *untergruppe_Fuehrerstandsanzeigen = new Node(0x0A);
-			for (int i = 0; i < requestListFuehrerstandsanzeigen->size(); i++) {
-				Attribute *attr = new Attribute(1, requestListFuehrerstandsanzeigen->get(i));
-				untergruppe_Fuehrerstandsanzeigen->addAttribute(attr);
-			}
-		befehl_NEEDED_DATA->addNode(untergruppe_Fuehrerstandsanzeigen);
-		if (reqFuehrerstandsbedienung) {
-			Node *untergruppe_Fuehrerstandsbedienung = new Node(0x0B);
-			befehl_NEEDED_DATA->addNode(untergruppe_Fuehrerstandsbedienung);
-		}
-		if (reqProgrammdaten) {
-			Node *untergruppe_Programmdaten = new Node(0x0C);
-			for (int i = 1; i <= 4; i++) {
-				Attribute *attribute = new Attribute(1, i);
-				untergruppe_Programmdaten->addAttribute(attribute);
-			}
-			befehl_NEEDED_DATA->addNode(untergruppe_Programmdaten);
-		}
-	client_Anwendung->addNode(befehl_NEEDED_DATA);
-	int length = 0;
-	byte *result = client_Anwendung->get(&length);
-	client->write(result, length);
-	delete result;
-	delete client_Anwendung;
-}
-
-void Zusi3Schnittstelle::ACK_NEEDED_DATA() {
-	while (client->available() < 10) {
-		delay(250);
-	}
-	byte *header = new byte[4];
-	client->read(header, 4);
-	byte *id = new byte[2];
-	client->read(id, 2);
-	Node *Client_Anwendung_02 = getNodes(id);
-	if (Client_Anwendung_02->getIDAsInt() == 2) {
-		Node *befehl_ACK_NEEDED_DATA = Client_Anwendung_02->getNodeByID(0x04);
-		if (befehl_ACK_NEEDED_DATA != NULL) {
-			int befehl_Aktzeptiert = befehl_ACK_NEEDED_DATA->getAttributeByID(1)->getDATAAsInt();
-			if (befehl_Aktzeptiert == 0) {
-				Serial.println("Befehl aktzeptiert");
-			}
-			else {
-				Serial.println("Befehl nicht aktzeptiert");
-			}
-		}
-	}
-	delete header;
-	delete Client_Anwendung_02;
-}
-
-Node *Zusi3Schnittstelle::getNodes(byte *rootID) {
-	Node *rootNode = new Node(rootID);
-	while (client->available() > 0) {
-		byte header[4];
-		client->read(header, 4);
-		if (compare(header, HEADER, 4)) {
-			byte *ID = new byte[2];
-			client->read(ID, 2);
-			Node *subNode = getNodes(ID);
-			rootNode->addNode(subNode);
-		}
-		else if (compare(header, ENDE, 4)) {
-			return rootNode;
-		}
-		else {
-			byte *ID = new byte[2];
-			client->read(ID, 2);
-			int length = ((header[0] & 0xFF) | (header[1] & 0xFF) << 8 | (header[2] & 0xFF) << 16 | (header[3] & 0xFF) << 24);
-			if (length - 2 > 0) {
-				byte *DATA = new byte[length - 2];
-				client->read(DATA, length - 2);
-				Attribute *attr = new Attribute(ID, DATA, length - 2);
-				rootNode->addAttribute(attr);
-			}
-		}
-	}
-	return rootNode;
-}
-
-boolean Zusi3Schnittstelle::compare(byte * a, byte * b, int size) {
-	for (int i = 0; i < size; i++) {
-		if (a[i] != b[i]) {
-			return false;
-		}
-	}
-	return true;
-}
+};
+#endif
